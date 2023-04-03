de25272f19840bde9726abd47a09dc8ff4b74b19
vladpsel <vpanovwr@gmail.com>
2023-04-03 13:26:30 +0300

    fix(config): fix webpack config for js+sass

--- webpack.config.js
+++ webpack.config.js
@@ -5,22 +5,35 @@ module.exports = {
   mode: 'development',
   watch: true,
   entry: [
+    __dirname + '/src/js/script.js',
     __dirname + '/src/sass/style.sass'
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'css/style.css',
+    filename: 'js/script.min.js',
   },
   module: {
    rules: [
+
+     {
+       test: /\.js$/,
+       exclude: /node_modules/,
+       use: [],
+     },
+
      {
        test: /\.s[ac]ss$/i,
+       exclude: /node_modules/,
        use: [
-         "style-loader",
-         "css-loader",
-         "sass-loader",
+         {
+             loader: 'file-loader',
+             options: { outputPath: 'css/', name: '[name].min.css'}
+         },
+         'sass-loader'
        ],
      },
+
+
    ],
   },
   plugins: [
@@ -29,6 +42,7 @@ module.exports = {
         { from: path.resolve(__dirname, 'src/css'), to: path.resolve(__dirname, 'dist/css'), noErrorOnMissing: true },
         { from: path.resolve(__dirname, 'src/font'), to: path.resolve(__dirname, 'dist/font'), noErrorOnMissing: true },
         { from: path.resolve(__dirname, 'src/img'), to: path.resolve(__dirname, 'dist/img'), noErrorOnMissing: true },
+        // { from: path.resolve(__dirname, 'src/js/bootstrap-5.3.0.min.js'), to: path.resolve(__dirname, 'dist/js'), noErrorOnMissing: true },
       ],
     }),
   ],
